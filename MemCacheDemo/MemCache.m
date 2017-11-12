91077d458ca927cff46edb2ea56eb5d0e532c5e9
godL <547188371@qq.com>
2017-11-12 14:08:42 +0800

    modify release callback

--- MemCacheDemo/MemCache.m
+++ MemCacheDemo/MemCache.m
@@ -51,7 +51,18 @@ static inline void CacheNodeRelease(void *ptr) {
     item = NULL;
 }
 
-- (id)_removeNode:(linkNode *)node {
+static inline void CacheBringNodeToHeader(linkList *list,linkNode *node) {
+    if (list->head == node) return;
+    if (node->prev) node->prev->next = node->next;
+    if (node->next) node->next->prev = node->prev;
+    if (list->tail == node) list->tail = node->prev;
+    list->head->prev = node;
+    node->prev = NULL;
+    node->next = list->head;
+    list->head = node;
+}
+
+- (void)_removeNode:(linkNode *)node {
     CFDictionaryRemoveValue(_cache_hash, NodeGetCacheKey(node));
     if (node->prev) node->prev->next = node->next;
     if (node->next) node->next->prev = node->prev;
@@ -59,15 +70,16 @@ static inline void CacheNodeRelease(void *ptr) {
     if (_cache_list->tail == node) _cache_list->tail = node->prev;
     _totalCost -= NodeGetCacheCost(node);
     _totalCount -- ;
-    id value = (__bridge id)NodeGetCacheValue(node);
-    free(node);
-    return value;
+    dispatch_async(_release_queue, ^{
+        CacheNodeRelease(node);
+        free(node);
+    });
 }
 
-- (id)_removeTailNode {
-    if (_totalCount == 0) return nil;
+- (void)_removeTailNode {
+    if (_totalCount == 0) return ;
     linkNode *node = _cache_list->tail;
-    return [self _removeNode:node];
+    [self _removeNode:node];
 }
 
 - (void)_removeAllNode {
@@ -155,6 +167,22 @@ static inline void CacheNodeRelease(void *ptr) {
         return;
     }
     dispatch_semaphore_wait(_lock, DISPATCH_TIME_FOREVER);
+    linkNode *node = (linkNode *)CFDictionaryGetValue(_cache_hash, (__bridge const void *)(key));
+    if (node) {
+        Cache_item *item = NodeGetCacheItem(node);
+        _totalCost -= NodeGetCacheItem(node)->cost;
+        _totalCount += cost;
+        item->cost = cost;
+        item->value = (__bridge void *)(obj);
+        CacheBringNodeToHeader(_cache_list, node);
+    }else {
+        Cache_item *item = CacheItemify((__bridge const void *)(key), (__bridge const void *)(obj), cost);
+        linkListAddHead(_cache_list, item);
+        CFDictionarySetValue(_cache_hash, (__bridge const void *)(key), _cache_list->head);
+        if (_totalCount > self.countLimit) {
+            [self _removeTailNode];
+        }
+    }
     
 }
 
@@ -170,10 +198,7 @@ static inline void CacheNodeRelease(void *ptr) {
     dispatch_semaphore_wait(_lock, DISPATCH_TIME_FOREVER);
     linkNode *node = (linkNode *)CFDictionaryGetValue(_cache_hash, (__bridge const void *)(key));
     if (node) {
-        id value = [self _removeNode:node];
-        dispatch_async(_release_queue, ^{
-            [value class];
-        });
+        [self _removeNode:node];
     }
     dispatch_semaphore_signal(_lock);
 }
--- MemCacheDemo/MemCacheDemo/AppDelegate.m
+++ MemCacheDemo/MemCacheDemo/AppDelegate.m
@@ -16,7 +16,6 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    NSCache *cache;
     // Override point for customization after application launch.
     return YES;
 }
